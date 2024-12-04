00d3e02e3c946e9f8777056ac2183b8ed3c0ed8e
CALLOFDUTY51 <rksharma97524@gmail.com>
2024-12-04 19:56:48 +0530

    i give up

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
-  return {
-    plugins: [react()],
-    define: {
-      __APP_ENV__: JSON.stringify(process.env.VITE_VERCEL_ENV), // Vercel environment variable
-      'process.env.VITE_CLOUDINARY_CLOUD_NAME': JSON.stringify(process.env.VITE_CLOUDINARY_CLOUD_NAME),
-      'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
-    },
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+})
