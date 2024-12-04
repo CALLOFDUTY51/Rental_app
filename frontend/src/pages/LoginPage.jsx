b74ab863c84f0cef7f2b89a5aa6fb157adc81192
CALLOFDUTY51 <rksharma97524@gmail.com>
2024-12-04 18:37:40 +0530

    cors updated again

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -14,7 +14,7 @@ function LoginPage() {
   const handleSumbit=async(e)=>{
     e.preventDefault()
     const apiUrl = import.meta.env.VITE_API_URL;  // Use Vite's environment variable syntax
-    const url = `${apiUrl}/auth/login`;
+    const url = `https://rental-app-vc3y.onrender.com/auth/login`;
     
       axios.post(url,{email,password}).then((res)=>{
         console.log(res.data)
